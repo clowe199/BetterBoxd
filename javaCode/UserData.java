042b388b69453f3a081678b9fac21aeb387ea651
tallk <tallk@10.8.21.135>
2022-11-09 17:07:39 -0600

    UserData updates with builder pattern and simple test for creating new UserData

---
+++ javaCode/UserData.java
@@ -0,0 +1,65 @@
+﻿package javaCode;
+
+public class UserData {
+    private char[] username;
+    private char[] password;
+    private String emailAddress;
+    
+    public UserData(UserDataBuilder builder){
+        username = builder.username;
+        password = builder.password;
+        emailAddress = builder.emailAddress;
+    }
+
+    public char[] getUsername(){
+        return username;
+    }
+    public char[] getPassword(){
+        return password;
+    }
+    public String getEmailAddress(){
+        return emailAddress;
+    }
+
+    public void setUsername(char[] u){
+        username = u;
+    }
+    public void setPassword(char[] p){
+        password = p;
+    }
+    public void setEmailAddress(String a){
+        emailAddress = a;
+    }
+
+    static public class UserDataBuilder {
+        private char[] username;
+        private char[] password;
+        private String emailAddress;
+        
+        public UserDataBuilder() {
+        	
+        }
+        
+        public UserDataBuilder username(char[] user)
+        {
+            this.username = user;
+            return this;
+        }
+    
+        public UserDataBuilder password(char[] pass)
+        {
+            this.password = pass;
+            return this;
+        }
+    
+        public UserDataBuilder emailAddress(String add)
+        {
+            this.emailAddress = add;
+            return this;
+        }
+    
+        public UserData build(){
+            return new UserData(this);
+        }
+    }
+}
